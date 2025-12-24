833404166edea5c0613f9f1cec9e7cad3cf3e5db
Johnythepony69 <163742685+Johnythepony69@users.noreply.github.com>
2025-12-24 16:09:46 +0100

    Update app.js

--- love-albums/public/app.js
+++ love-albums/public/app.js
@@ -1,1 +1,9 @@
 // frontend logic
+const firebaseConfig = {
+  apiKey: "AIzaSyBxrU33AhmFJ8wSjGYCCnX_vxOoI7MaMlg",
+  authDomain: "love-albums.firebaseapp.com",
+  projectId: "love-albums",
+  storageBucket: "love-albums.firebasestorage.app",
+  messagingSenderId: "100992494852",
+  appId: "1:100992494852:web:c4bbb406bb506ea6bab451"
+};
